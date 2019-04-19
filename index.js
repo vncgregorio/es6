e5f297f72bae4ce1e0789b4b6e3ef0f4b69c512f
Vinícius Gregório <vinicius.gregorio@optera.com.br>
2019-04-19 18:45:14 -0300

    primeiros testes em es6

---
+++ index.js
@@ -0,0 +1,53 @@
+class Message {
+  constructor(text = '', created = Date.now()){
+    this.text = text;
+    this.created = created;
+  }
+  get created(){
+    return this._created;
+  }
+  set created(created){
+    if (!created || isNaN(created)) {
+      throw new Error('Invalid created');
+    }
+    this._created = created;
+  }
+  toString() {
+    return `Message created at: ${this.created} - Text: ${this.text}`;
+  }
+}
+
+class ImageMessage extends Message {
+  constructor(text = '',
+    created = Date.now(),
+    url = '',
+    thumbnail = ''
+  ){
+    super(text, created);
+    this.url = url;
+    this.thumbnail = thumbnail;
+  }
+  toString() {
+    return `Photo: ${super.toString()}` +
+    `- URL: ${this.url}` +
+    `thumbnail: ${this.thumbnail}`;
+  }
+}
+
+
+
+var emptyMessage = new Message();
+var textMessage = new Message('Yesterday message', Date.now() - 86400);
+var photoMessage = new ImageMessage();
+
+console.log(String(emptyMessage));
+console.log(String(textMessage));
+console.log(String(photoMessage));
+
+console.log(emptyMessage instanceof Message);
+console.log(textMessage instanceof Message);
+console.log(photoMessage instanceof Message);
+console.log(photoMessage instanceof ImageMessage);
+
+console.log(emptyMessage instanceof ImageMessage);
+console.log(textMessage instanceof ImageMessage);
